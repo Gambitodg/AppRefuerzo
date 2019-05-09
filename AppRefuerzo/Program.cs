ed35c48597e6aaaa4d27689e79a18b8febe13164
Gambitodg <50213808+Gambitodg@users.noreply.github.com>
2019-05-09 14:09:49 -0500

    Initial commit

---
+++ AppRefuerzo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRefuerzo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, apellido;
+            bool estado = false;
+            var edad =0;
+            var resultado = 0.0;
+
+            Console.Write("Digite la edad: ");
+            edad = int.Parse(Console.ReadLine());
+            Console.Write("Digite el nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine($"Bienvenido \n Su nombre es:{nombre} \n su edad es: {edad}");
+            Console.WriteLine("Bienvenio Su nombre es:" + nombre + "Sue edad es:" + edad);
+            Console.WriteLine("Bienvenido su nombre es:{0} y su edad es {1}",nombre,edad);
+
+            if (estado==false)
+            {
+                edad+=50;
+            }
+            else
+            {
+              resultado = Math.Sqrt(edad);
+
+                Console.Write($"La raiz cuadrada de edad:{resultado}");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ ConicionalSwitch/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConicionalSwitch
+{
+    class Program
+    {
+        //Elaborar un programa que me permita seleccionar operaciones a realizar
+        //Opción 1. Raiz
+        //Opción 2. Potenciación
+        //Opción 3. Valor Absoluto
+        //Opcion 4. Cuadrado
+        //Opción 5. Salir        
+        // Si la opción no se encuentra dentro de estas generar mensaje de error
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Opción 1. Raiz \n Opción 2. Potenciación \n Opción 3. Valor Absoluto \n Opcion 4. Cuadrado \n Opción 5. Salir");
+            
+            Console.WriteLine("Ingrese la opción:");
+            var opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Digite número:");
+                    var numero = int.Parse(Console.ReadLine());
+                    Math.Sqrt(numero);
+                break;
+                    
+                case 2:
+                    Console.WriteLine(" Digite la base:" );
+                    var bas = int.Parse(Console.ReadLine());
+                    Console.WriteLine(" Digite el exponente:");
+                    var exponente = int.Parse(Console.ReadLine());
+                    Math.Pow(bas,exponente);
+                break;
+
+                case 3:
+                    Console.WriteLine("Digite número:");
+                    var numeroabs = int.Parse(Console.ReadLine());
+                    Math.Abs(numeroabs);
+                break;
+
+                case 4:
+                    Console.WriteLine("Digite número:");
+                    var numerocua = int.Parse(Console.ReadLine());
+                    numerocua *= numerocua;
+                break;
+
+                case 5:
+                    Console.ReadLine();
+                break;
+
+                default:
+                    Console.WriteLine("No has seleccionado ninguna opción");
+                break;
+            }
+        }
+
+    }
+}
